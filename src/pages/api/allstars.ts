7aed980191fccad565b209996876fcc994ae3be8
Wojciech Kochanowski <wojciech.kochanowski@o2.pl>
2023-04-18 17:03:47 +0200

    api debug

--- src/pages/api/allstars.ts
+++ src/pages/api/allstars.ts
@@ -18,7 +18,7 @@ export default async function allStars(
       },
     })
     res.status(200).json(stars)
-  } catch (error) {
-    res.status(503)
+  } catch (error: any) {
+    res.status(200).json(error)
   }
 }
